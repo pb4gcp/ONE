56aa77c6bfb927e06c73d9a1084cb22c8c9318d3
Prashant Bagwe <pb4gcp@gmail.com>
2019-06-15 23:56:00 +0530

    Added Greetings

--- src/main/java/com/ci/one/App.java
+++ src/main/java/com/ci/one/App.java
@@ -1,5 +1,8 @@
 package com.ci.one;
 
+import java.time.format.DateTimeFormatter;  
+import java.time.LocalDateTime;    
+
 /**
  * Hello world!
  *
@@ -9,5 +12,22 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("HH");
+        String hour = dtf.format(LocalDateTime.now());
+        int hr = Integer.parseInt(hour);
+        String greet = "Good ??";
+        
+        if (hr < 12) {
+        	greet = "Good Morning";
+        } else if (hr < 16) {
+        	greet = "Good Afternoon";
+        } else if (hr < 20) {
+        	greet = "Good Evening";
+        } else {
+        	greet = "Good Night";
+        }
+        
+        System.out.println(greet);
     }
 }
